4a997223680c5bd106b60b065c81e4f075351c4a
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-10-08 08:11:23 +0800

    added some

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // problem : 1
+
+
 const mainFunctoions = [`I am a web developer ,
 i love to code,
 I love to eat biriani`]
-// console.log(...mainFunctoions);
+console.log(...mainFunctoions);
+
+
+function main(first, second) {
+    const total = first + second;
+    return total
+}
+const result = main(20,10);
+console.log(result);
+
+/*
+    Thired task
+*/
+
+function squredNumber(square) {
+    return square ** 3;
+}
+const finedThere = squredNumber(3);
+console.log(finedThere)
 
 
